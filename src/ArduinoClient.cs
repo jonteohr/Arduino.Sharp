a6c6872c062ab4880e0f5c854c7c4ac4e4d7868c
jonteohr <jonathan.ohrstrom@saabgroup.com>
2024-04-23 13:03:17 +0200

    init code

---
+++ src/ArduinoClient.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+
+namespace Arduino.Sharp
+{
+    /// <summary>
+    /// The base client of communication
+    /// </summary>
+    public class ArduinoClient
+    {
+        /// <summary>
+        /// The configured portname we want to communicate to
+        /// </summary>
+        public string PortName => IsConnected ? m_serialPort.PortName : string.Empty;
+        /// <summary>
+        /// The configured baudrate we're expecting
+        /// </summary>
+        public int BaudRate => IsConnected ? m_serialPort.BaudRate : -1;
+        /// <summary>
+        /// If the communication to the port is open or closed.
+        /// </summary>
+        public bool IsConnected => m_serialPort.IsOpen;
+
+        /// <summary>
+        /// Called whenever we receive data on the configured port.
+        /// </summary>
+        public event EventHandler<string> DataReceived;
+
+        private readonly SerialPort m_serialPort;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="portName"></param>
+        /// <param name="baudRate"></param>
+        /// <exception cref="IOException"></exception>
+        public ArduinoClient(string portName, int baudRate)
+        {
+            try
+            {
+                m_serialPort = new SerialPort
+                {
+                    PortName = portName,
+                    BaudRate = baudRate
+                };
+                m_serialPort.Open();
+
+                m_serialPort.DataReceived += SerialPortOnDataReceived; 
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw new IOException("Could not establish connection with port " + portName, ex.InnerException);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <exception cref="Exception"></exception>
+        public void SendData(string message)
+        {
+            if (IsConnected)
+                m_serialPort.Write(message);
+            else
+                throw new Exception("Not connected"); // TODO Create custom exception for connectivity issues
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <exception cref="Exception"></exception>
+        public void SendData(byte[] buffer, int offset, int count)
+        {
+            if(IsConnected)
+                m_serialPort.Write(buffer, offset, count);
+            else
+                throw new Exception("Not connected"); // TODO Create custom exception for connectivity issues
+        }
+        
+        
+        private void SerialPortOnDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var data = m_serialPort.ReadExisting();
+            
+            DataReceived?.Invoke(sender, data);
+        }
+    }
+}
---
+++ src/ArduinoClientBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Arduino.Sharp
+{
+    /// <summary>
+    /// Builder class for setting up the main client.
+    /// </summary>
+    public class ArduinoClientBuilder
+    {
+        private int m_baudRate;
+        /// <summary>
+        /// Sets the baudrate for communication to the board.
+        /// </summary>
+        /// <param name="baudRate">The baudrate</param>
+        /// <example>9600</example>
+        /// <returns>The client builder instance</returns>
+        public ArduinoClientBuilder SetBaudRate(int baudRate)
+        {
+            m_baudRate = baudRate;
+            return this;
+        }
+
+        private string m_portName = string.Empty;
+        /// <summary>
+        /// Sets the name of the serial port on the computer that is connected to the board. 
+        /// </summary>
+        /// <param name="portName">The name of the COM-port</param>
+        /// <example>COM4</example>
+        /// <returns>The client builder instance</returns>
+        public ArduinoClientBuilder SetPortName(string portName)
+        {
+            m_portName = portName;
+            return this;
+        }
+
+        /// <summary>
+        /// Builds the ArduinoClient with specified settings
+        /// </summary>
+        /// <returns>Built <see cref="ArduinoClient"/> with the settings specified</returns>
+        /// <exception cref="ArgumentException">If required settings have not been set</exception>
+        public ArduinoClient Build()
+        {
+            if (m_baudRate == 0 || m_portName == string.Empty)
+                throw new ArgumentException("Not enough settings passed");
+            
+            return new ArduinoClient(m_portName, m_baudRate);
+        }
+    }
+}
